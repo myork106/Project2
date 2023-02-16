04505786fb0551dcfedf18b0c6f65259139a21f4
myork106 <124708010+myork106@users.noreply.github.com>
2023-02-16 18:02:48 -0500

    Create servo.h

---
+++ modules/servo/servo.h
@@ -0,0 +1,14 @@
+//=====[#include guards - begin]===============================================
+
+#ifndef _SERVO_H_
+#define _SERVO_H_
+
+//=====[Declarations (prototypes) of public functions]=========================
+
+void servoInit();
+void servoOpen();
+void servoClose();
+
+//=====[#include guards - end]=================================================
+
+#endif // _SERVO_H_
